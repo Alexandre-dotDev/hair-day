937311b28ca1b9500a09471fbd29c03d7ed519a2
Alexandre-dotDev <alexandre.centro@yahoo.com.br>
2025-05-21 15:48:26 -0300

    add style-loader e css-loader e configurações

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
- 
+"use strict";
+
+//CSS
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/schedule.css";
--- webpack.config.js
+++ webpack.config.js
@@ -38,4 +38,13 @@ module.exports = {
       favicon: path.resolve("src", "assets", "scissors.svg"),
     }),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
