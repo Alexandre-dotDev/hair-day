e27b75bc8d5ec2a0383c5e70ad1e2cd21244bf09
Alexandre-dotDev <alexandre.centro@yahoo.com.br>
2025-05-21 12:13:29 -0300

    Instalando e configurando o webpack-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,24 @@ module.exports = {
   target: "web",
   mode: "development",
 
-  // É recomendando usar o path, pois ele irá tratar as particularidades de caminhos de acordo com o sistema operacional.
+  /*
+   É recomendando usar o path, pois ele irá tratar as particularidades de caminhos de acordo com o sistema operacional.
+  */
   entry: path.resolve(__dirname, "src", "main.js"),
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+
+  /*
+    Cria a conecão com servido local na porta 3000, utilizando o webpack serve. A sintax precisar ser iagual a da documentção, o codigo abaixo é extraido do site do webpack.
+  */
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000, //Indica a porta
+    open: true, //Abre uma aba automaticamente no browser quando conectado
+    liveReload: true, //Atualiza automaticamente a cada alteração, sem a necessidade de desconectar com o servidor.
   },
 };
