3db6707db52edc53cdd1effd3a0cc00e182a1598
Alexandre-dotDev <alexandre.centro@yahoo.com.br>
2025-05-21 15:04:59 -0300

    Instalando e configurando o html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
+// Constante que import o path, que será utilizado para tratar os caminhos de diversos sistemas operacionais.
 const path = require("path");
+// Importação do plugin
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+/*
+  Cria um modulo de exportação para expor os diretórios src e dist.
+*/
 module.exports = {
   target: "web",
   mode: "development",
@@ -7,14 +13,15 @@ module.exports = {
   /*
    É recomendando usar o path, pois ele irá tratar as particularidades de caminhos de acordo com o sistema operacional.
   */
-  entry: path.resolve(__dirname, "src", "main.js"),
+  entry: path.resolve(__dirname, "src", "main.js"), //Entrada - dados que serão empacotados pelo webpack a partir da pasta src e do arquivo main.js.
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-  },
+  }, //Saída - cria a pasta dist com um arquivo empacotados que se chamará main.js.
 
   /*
-    Cria a conecão com servido local na porta 3000, utilizando o webpack serve. A sintax precisar ser iagual a da documentção, o codigo abaixo é extraido do site do webpack.
+    Rodar um servidor de desenvolvimento com hot reload (recarga automática ao salvar arquivos).
   */
   devServer: {
     static: {
@@ -24,4 +31,10 @@ module.exports = {
     open: true, //Abre uma aba automaticamente no browser quando conectado
     liveReload: true, //Atualiza automaticamente a cada alteração, sem a necessidade de desconectar com o servidor.
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
 };
