668602fb039e4a330553e166a8beaff041170d6b
Alexandre-dotDev <alexandre.centro@yahoo.com.br>
2025-05-23 13:52:06 -0300

    add Dayjs e funcionalidade ao input date

---
+++ src/modules/form/submit.js
@@ -0,0 +1,17 @@
+import dayjs from "dayjs";
+
+const form = document.querySelector("form");
+const selectedDate = document.querySelector("#date");
+
+// Data atual para formatar o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
+
+// Carrega a data atual e define a data mínima com sendo a data atual.
+selectedDate.value = inputToday;
+selectedDate.min = inputToday;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log("Enviado");
+});
