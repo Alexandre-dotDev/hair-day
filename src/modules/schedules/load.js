a766ef72d9cb2655b2ab24eeabfcdf2639e44031
Alexandre-dotDev <alexandre.centro@yahoo.com.br>
2025-05-26 22:33:16 -0300

    Add function de busca de agendamentos

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,12 +1,17 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 //Seleciona o input de data.
 const selectedDate = document.querySelector("#date");
 // console.log(selectedDate);
 
-export function schedulesDay() {
+export async function schedulesDay() {
   //Obtém a data do input.
   const date = selectedDate.value;
+
+  //Busca na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+
   //Renderiza as horas dísponiveis.
   hoursLoad({ date });
 
